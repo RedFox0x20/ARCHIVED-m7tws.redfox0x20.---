2fae96b84a1c4b8c71737623d9cec5a04f148648
Red (RedFox0x20) <redfox0x20@gmail.com>
2021-09-02 18:35:54 +0100

    Basics of the m7tws page

--- components/header.js
+++ components/header.js
@@ -1,12 +1,19 @@
 import Link from 'next/link';
 
-const NavbarLinks = [{ text: 'Home', url: '/' }];
+const NavbarLinks = [
+    { text: 'Home', url: 'https://www.redfox0x20.xyz/' },
+    { text: 'Amateur Radio', url: '/' },
+    { text: 'NOAA Gallery', url: '/wx-captures' },
+    // { text: 'SSTV Gallery', url: '/sstv-captures' }
+];
 
 export default function Header() {
     return (
         <div className="navbar mb-2 shadow-lg bg-neutral text-neutral-content lg:sticky lg:top-0 lg:z-50 drop-shadow-2xl">
             <div className="flex-none px-2 mx-2">
-                <span className="text-lg font-bold">{'RedFox0x20'}</span>
+                <span className="text-lg font-bold">
+                    {'RedFox0x20 | M7TWS'}
+                </span>
             </div>
             <div className="flex-1 px-2 mx-2">
                 <div className="items-stretch hidden md:flex">
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 
+import { CardButton, CardTitle, ProjectCard } from '../components/cards';
 import {
     ContentSection,
     MainSection,
@@ -7,16 +8,18 @@ import {
     SectionDescription,
     SectionTitle,
 } from '../components/sections';
+import DynamicGrid from '../components/dynamicgrid';
 import Footer from '../components/footer';
 import Header from '../components/header';
+import Link from 'next/link';
 
 export default function Home() {
     return (
         <div>
             <Head>
-                <title>{'RedFox0x20'}</title>
+                <title>{'M7TWS'}</title>
                 <meta
-                    content="RedFox0x20 | Website description"
+                    content="M7TWS Amateur Radio | HF VHF Operations | Satellite Reception"
                     name="description"
                 />
                 <link href="/favicon.ico" rel="icon" />
@@ -26,20 +29,98 @@ export default function Home() {
 
             <MainSection>
                 <ContentSection>
-                    <SectionTitle>{'Section title'}</SectionTitle>
+                    <SectionTitle>{'M7TWS'}</SectionTitle>
                     <SectionContent>
                         <SectionDescription>
-                            {'Section Description'}
+                            {'UK Amateur Radio Operator'}
+                            <Link href="https://qrz.com/db/M7TWS" passHref>
+                                <span className="btn btn-sm btn-link">
+                                    {'View on QRZ.com'}
+                                </span>
+                            </Link>
                         </SectionDescription>
+                        <p>
+                            {
+                                "Hi there! I've been an amateur radio operator since the 12th of October 2020, I passed my Foundation exam via an online exam on the 7th of October 2020."
+                            }
+                            <br />
+                            {
+                                'Most of my operation takes place on the 2 Metre band however I also enjoy some HF SSB and Data.'
+                            }
+                        </p>
+                        <div className="mt-4">
+                            <p className="underline">{'Transceivers'}</p>
+                            <ul className="ml-8 list-disc">
+                                <li>
+                                    {
+                                        'TR-9000 2 Metre All Mode Transceiver (With DIY Data-mode Interface)'
+                                    }
+                                </li>
+                                <li>
+                                    {
+                                        'Icom IC-735 HF All Mode Transceiver (With DIY Data-mode Interface)'
+                                    }
+                                </li>
+                            </ul>
+                            <p className="underline">
+                                <br />
+                                {'Antennas'}
+                            </p>
+                            <ul className="ml-8 list-disc">
+                                <li>
+                                    {
+                                        'DP2-2 Commercial dipole for 2 Metres - Variable length'
+                                    }
+                                </li>
+                                <li>
+                                    {'DIY 3 Element 2 Metre Yagi-Uda built by '}
+                                    <span className="link link-primary">
+                                        <Link
+                                            href="https://qrz.com/db/M7JRF"
+                                            passHref
+                                        >
+                                            {'James, M7JRF'}
+                                        </Link>
+                                    </span>
+                                </li>
+                            </ul>
+                            <p className="underline">
+                                <br />
+                                {'Power supplies'}
+                            </p>
+                            <ul className="ml-8 list-disc">
+                                <li>
+                                    {'Jetfon PC30SWM 30 Amp 13.8V Mains PSU'}
+                                </li>
+                                <li>
+                                    {'Nevada PS204 3-6 Amp 13.8V Mains PSU'}
+                                </li>
+                                <li>{'17Ah Lead Acid Battery for /P'}</li>
+                            </ul>
+                        </div>
                     </SectionContent>
                 </ContentSection>
                 <div className="divider" />
                 <ContentSection>
-                    <SectionTitle>{'Section two'}</SectionTitle>
+                    <SectionTitle>{'Image Gallery'}</SectionTitle>
                     <SectionContent>
                         <SectionDescription>
-                            {'Another description'}
+                            {
+                                "Here's a collection of images that I have received via NOAA APT Weather Satellites"
+                            }
                         </SectionDescription>
+                        <DynamicGrid>
+                            <ProjectCard altTxt="" imgSrc="">
+                                <CardTitle>{'NOAA Satellite image'}</CardTitle>
+                                <CardButton href="/wx-captures">
+                                    {'Open NOAA Gallery'}
+                                </CardButton>
+                            </ProjectCard>
+                            {/* <ProjectCard imgSrc='' altTxt=''>
+                                <CardTitle>SSTV image</CardTitle>
+                                <CardButton href='/sstv-captures'>Open SSTV Gallery</CardButton>
+                            </ProjectCard> */}
+                        </DynamicGrid>
                     </SectionContent>
                 </ContentSection>
             </MainSection>
