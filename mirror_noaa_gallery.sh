aa70d70652012e935c54cbdf6a41e751d9cdb733
Red (RedFox0x20) <redfox0x20@gmail.com>
2021-09-03 22:34:44 +0100

    Update scripts

--- mirror_noaa_gallery.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /srv/www/m7tws.redfox0x20.xyz/public/wx-captures/
-while true; do wget -mr ftp://radio:@192.168.1.127/* -A jpg -nH --cut-dirs=1 && sleep 15m; done
--- restart_hook.sh
+++ restart_hook.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-while true; do inotifywait -r /srv/www/m7tws.redfox0x20.xyz/public/wx-captures && pm2 stop m7tws && sleep 30s && pm2 start m7tws; done
+while true; do
+ pm2 stop m7tws
+ cd /srv/www/m7tws.redfox0x20.xyz/public/wx-captures/
+ wget -mr ftp://radio:@192.168.1.127/* -A jpg -nH --cut-dirs=
+ pm2 start m7tws
+ sleep 15m
+done
