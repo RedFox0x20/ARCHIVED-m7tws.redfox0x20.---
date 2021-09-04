729dba16a91bc130a0ace95533085ba14dfec3c8
Red (RedFox0x20) <redfox0x20@gmail.com>
2021-09-04 11:24:36 +0100

    update scripts;

--- restart_hook.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-while true; do
- pm2 stop m7tws
- cd /srv/www/m7tws.redfox0x20.xyz/public/wx-captures/
- wget -mr ftp://radio:@192.168.1.127/* -A jpg -nH --cut-dirs=
- pm2 start m7tws
- sleep 15m
-done
--- start.sh
+++ start.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-cd /srv/www/m7tws.redfox0x20.xyz/public/wx-captures/
-wget -mr ftp://radio:@192.168.1.127/* -A jpg -nH --cut-dirs=
-
 cd /srv/www/m7tws.redfox0x20.xyz
 /usr/bin/git pull
 /usr/bin/yarn production
